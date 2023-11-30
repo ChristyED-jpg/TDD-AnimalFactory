922c681cf71cf7c89d4001b5834344eb3c73d3d7
Christy <christy@Zipcoders-MacBook-Pro-90.local>
2023-11-30 18:21:36 -0500

    done with AnimalFactoryTets

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,56 @@
 package rocks.zipcodewilmington;
 
+import com.thoughtworks.qdox.model.BeanProperty;
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
+ *
  */
+
+
 public class AnimalFactoryTest {
-    //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
-    //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+  //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
+  @Test
+  public void testAnimalCreateDog() {
+    // Given
+
+    String name = "";
+    Date birthDate = new Date();
+
+    //When
+
+    Dog dog =  AnimalFactory.createDog(name, birthDate);
+
+    // Then
+     Assert.assertNotNull( dog );
+
+  }
+
+
+  //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+  @Test
+  public void testAnimalCreateCat(){
+
+    //Given
+    String name = "";
+    Date birthDate = new Date();
+
+    //When
+    Cat cat = AnimalFactory.createCat(name, birthDate);
+
+    // then
+    Assert.assertNotNull(cat);
+
+  }
+
+
 }
--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,10 +1,22 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+
 /**
  * @author leon on 4/19/18.
  */
 public class CatHouseTest {
+
     // TODO - Create tests for `void add(Cat cat)`
+
+    @Test
+    public void add(Cat cat){
+
+    }
+
+
+
     // TODO - Create tests for `void remove(Integer id)`
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
